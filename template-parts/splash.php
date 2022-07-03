36a2f220565e4e21816961a8185bf67949b7619a
3rtea <7ove3orld@gmail.com>
2022-07-03 17:58:04 +0200

    This is the end

--- template-parts/splash.php
+++ template-parts/splash.php
@@ -20,25 +20,25 @@
 </script>
 <div class="title-splash splash-screen left-panel">
   <!-- 2sec max OU click -->
-  <a class="btn-img" href="javascript:splashScreenDone();">
+  <a class="btn-img" href="javascript:console.log('splashScreenDone();');">
     <img src="<?php echo get_template_directory_uri(); ?>/assets/img/title.webp" alt="Wolfdog Title Left">
   </a>
 </div>
 <div class="title-splash splash-screen right-panel">
   <!-- 2sec max OU click -->
-  <a class="btn-img" href="javascript:splashScreenDone();">
+  <a class="btn-img" href="javascript:console.log('splashScreenDone();');">
     <img src="<?php echo get_template_directory_uri(); ?>/assets/img/title.webp" alt="Wolfdog Title Right">
   </a>
 </div>
 <div class="logo-splash splash-screen left-panel">
   <!-- 2sec max OU click -->
-  <a class="btn-img" href="javascript:splashScreenDone();">
+  <a class="btn-img" href="javascript:console.log('splashScreenDone();');">
     <img src="<?php echo get_template_directory_uri(); ?>/assets/img/logo.webp" alt="Wolfdog Left">
   </a>
 </div>
 <div class="logo-splash splash-screen right-panel">
   <!-- 2sec max OU click -->
-  <a class="btn-img" href="javascript:splashScreenDone();">
+  <a class="btn-img" href="javascript:console.log('splashScreenDone();');">
     <img src="<?php echo get_template_directory_uri(); ?>/assets/img/logo.webp" alt="Wolfdog Right">
   </a>
 </div>
