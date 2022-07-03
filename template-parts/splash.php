69a5d3608c4e9f6096e0553276f1278fc8bfa257
3rtea <7ove3orld@gmail.com>
2022-07-03 18:29:38 +0200

    Contournement du problème de chargement sur IPhone

--- template-parts/splash.php
+++ template-parts/splash.php
@@ -17,28 +17,34 @@
       removeSplashScreen("title-splash");
     }, 777);
   }
+
+  function splashScreenDoneForced() {
+    splashScreenDone();
+    alert("Les vidéos ne sont pas encore chargées.");
+  }
+
 </script>
 <div class="title-splash splash-screen left-panel">
   <!-- 2sec max OU click -->
-  <a class="btn-img" href="javascript:console.log('splashScreenDone();');">
+  <a class="btn-img" href="javascript:splashScreenDoneForced();">
     <img src="<?php echo get_template_directory_uri(); ?>/assets/img/title.webp" alt="Wolfdog Title Left">
   </a>
 </div>
 <div class="title-splash splash-screen right-panel">
   <!-- 2sec max OU click -->
-  <a class="btn-img" href="javascript:console.log('splashScreenDone();');">
+  <a class="btn-img" href="javascript:splashScreenDoneForced();">
     <img src="<?php echo get_template_directory_uri(); ?>/assets/img/title.webp" alt="Wolfdog Title Right">
   </a>
 </div>
 <div class="logo-splash splash-screen left-panel">
   <!-- 2sec max OU click -->
-  <a class="btn-img" href="javascript:console.log('splashScreenDone();');">
+  <a class="btn-img" href="javascript:splashScreenDoneForced();">
     <img src="<?php echo get_template_directory_uri(); ?>/assets/img/logo.webp" alt="Wolfdog Left">
   </a>
 </div>
 <div class="logo-splash splash-screen right-panel">
   <!-- 2sec max OU click -->
-  <a class="btn-img" href="javascript:console.log('splashScreenDone();');">
+  <a class="btn-img" href="javascript:splashScreenDoneForced();">
     <img src="<?php echo get_template_directory_uri(); ?>/assets/img/logo.webp" alt="Wolfdog Right">
   </a>
 </div>
