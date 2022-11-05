9c6fac8944146e3bcca8142490284058a8d3ae33
3rtea <7ove3orld@gmail.com>
2022-11-05 14:38:06 +0100

    Fix IPhone bug

--- template-parts/splash.php
+++ template-parts/splash.php
@@ -39,24 +39,24 @@
 <div class="title-splash splash-screen left-panel">
   <!-- 2sec max OU click -->
   <a class="btn-img" href="javascript:splashScreenDoneForced();">
-    <img src="<?php echo get_template_directory_uri(); ?>/assets/img/title.webp" alt="Wolfdog Title Left">
+    <img src="<?php echo get_template_directory_uri(); ?>/assets/img/title.png" alt="Wolfdog Title Left">
   </a>
 </div>
 <div class="title-splash splash-screen right-panel">
   <!-- 2sec max OU click -->
   <a class="btn-img" href="javascript:splashScreenDoneForced();">
-    <img src="<?php echo get_template_directory_uri(); ?>/assets/img/title.webp" alt="Wolfdog Title Right">
+    <img src="<?php echo get_template_directory_uri(); ?>/assets/img/title.png" alt="Wolfdog Title Right">
   </a>
 </div>
 <div class="logo-splash splash-screen left-panel">
   <!-- 2sec max OU click -->
   <a class="btn-img" href="javascript:splashScreenDoneForced();">
-    <img src="<?php echo get_template_directory_uri(); ?>/assets/img/logo.webp" alt="Wolfdog Left">
+    <img src="<?php echo get_template_directory_uri(); ?>/assets/img/logo.png" alt="Wolfdog Left">
   </a>
 </div>
 <div class="logo-splash splash-screen right-panel">
   <!-- 2sec max OU click -->
   <a class="btn-img" href="javascript:splashScreenDoneForced();">
-    <img src="<?php echo get_template_directory_uri(); ?>/assets/img/logo.webp" alt="Wolfdog Right">
+    <img src="<?php echo get_template_directory_uri(); ?>/assets/img/logo.png" alt="Wolfdog Right">
   </a>
 </div>
